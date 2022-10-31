6313c987040c9c5b6f5b170308e1f526e73d16e9
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-10-31 13:01:38 +0530

    Update novalnetSepa.js

--- resources/js/novalnetSepa.js
+++ resources/js/novalnetSepa.js
@@ -8,36 +8,4 @@ jQuery(document).ready( function() {
     jQuery('#novalnet_form').on('submit',function(){
         jQuery('#novalnet_form_btn').attr('disabled',true);
     });
-    // Save account details process
-    if(jQuery("#nn_toggle_form").length <= 0) {
-        jQuery("#nn_load_new_form").show();
-    } else {
-        jQuery("#nn_load_new_form").hide();
-    }               
-    
-    if(jQuery("input[name='nn_radio_option']").length > 0) { 
-        var token = jQuery("input[name='nn_radio_option']:first").val(); 
-        if(token){
-            jQuery('#nn_customer_selected_token').val(token);
-        } else {
-            jQuery('#nn_customer_selected_token').val('');
-        }
-    }
-
-    jQuery("input[name='nn_radio_option']").on('click', function () {
-        var tokenValue = jQuery(this).val();
-        if(tokenValue){
-            jQuery('#nn_customer_selected_token').val(tokenValue);
-        } else {
-            jQuery('#nn_customer_selected_token').val('');
-        }
-        
-        if(jQuery(this).attr('id') == 'nn_toggle_form') {
-            jQuery("#nn_load_new_form").show();
-            jQuery('#nn_customer_selected_token').val('');                
-        } else {
-            jQuery("#nn_load_new_form").hide();                
-        }
-    });
-    jQuery("input[name='nn_radio_option']:first").attr("checked","checked");  
 });
