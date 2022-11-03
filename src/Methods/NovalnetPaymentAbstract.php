cf964c3dba166b0db9e128579c0fd6d4f3294528
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-03 14:43:31 +0530

    Update NovalnetPaymentAbstract.php

--- src/Methods/NovalnetPaymentAbstract.php
+++ src/Methods/NovalnetPaymentAbstract.php
@@ -143,7 +143,7 @@ abstract class NovalnetPaymentAbstract extends PaymentMethodBaseService
      */
     public function getFee(): float
     {
-        return 0.00;
+        return 1.00;
     }
 
     /**
@@ -190,7 +190,7 @@ abstract class NovalnetPaymentAbstract extends PaymentMethodBaseService
      */
     public function isSwitchableTo($orderId = null): bool
     {
-        if($orderId > 0) {
+        if($orderId > 0 && $this->isActive() == true) {
             return true;
         }
         return false;
