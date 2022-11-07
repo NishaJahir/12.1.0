71c22a78da0ac2c7796d544d5c5c5cef0252e0dd
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-07 12:04:05 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1275,8 +1275,8 @@ class PaymentService
     {
 	$dataBase = pluginApp(DataBase::class);
         // Get transaction details from the Novalnet database table
-        $transactionDetails = $dataBase->query(TransactionLog::class)->where('paymentName', 'like', '%novalnet_instalment%')->where('orderNo', '=', $orderNo)->get();
-        $transactionDetails = json_decode(json_encode($transactionDetails), true);
+        $transactionDetails = $dataBase->query(TransactionLog::class)->where('paymentName', 'like', '%novalnet_instalment%')->where('orderNo', '=', $orderNo)->limit(1)->get();
+        $transactionDetails = json_decode(json_encode($transactionDetails[0]), true);
 	$this->getLogger(__METHOD__)->error('ins11', $transactionDetails);
 	foreach($transactionDetails as $transactionDetailKey => $transactionDetail) {
 	    if(!empty($transactionDetail['instalmentInfo'])) {
